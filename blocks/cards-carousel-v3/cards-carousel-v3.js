91faf0fc3f94906ca9022a7426227ce420a41be4
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-06-10 16:15:04 +0530

    asdfgbn

--- blocks/cards-carousel-v3/cards-carousel-v3.js
+++ blocks/cards-carousel-v3/cards-carousel-v3.js
@@ -1,7 +1,5 @@
-import SwiperBlock from "../swiper/swiper.js"
 export default function decorate(block) {
   Array.from(block.children).forEach((element, index) => {
     element.classList.add("cards-carousel inner-" + (index + 1))
   })
-  
-}
+} 
