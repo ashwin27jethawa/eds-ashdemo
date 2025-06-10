7f1bfc6e99aeb2b7ac598b1052cd47a5a0354d54
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-06-10 15:11:16 +0530

    asdcvsdcfasd

--- blocks/hero-block-v3/hero-block-v3.js
+++ blocks/hero-block-v3/hero-block-v3.js
@@ -1,6 +1,11 @@
 import SwiperBlock from "../swiper/swiper.js"
 
-export default function decorate(block){
-    console.log(block);
-    SwiperBlock(block)
+export default function decorate(block) {
+  Array.from(block.children).forEach((element, index) => {
+    element.classList.add("inner-" + (index+1))
+    Array.from(element.children).forEach((element, indjx) => {
+      element.classList.add("inner-" + (index+1) + "-innerSub-" + (indjx+1))
+    })
+  })
+  SwiperBlock(block)
 }
