1526a136fe71db574353380f1ce2f88326ae8dd1
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-06-16 12:39:16 +0530

    sdfgbhzxcasdfasd

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -46,15 +46,21 @@ export default function decorate(block) {
                 }
                 
             }else if (itemclass == "slidepreview") {
-                swiperPagination = document.createElement("div");
-                swiperPagination.classList.add("swiper-pagination");
-                block.append(swiperPagination);
+                leftprevbtn = document.createElement("div");
+                leftprevbtn.classList.add("swiper-button-prev")
+                block.append(leftprevbtn)
 
-                if (decryptObj[itemclass]['pagination'] != undefined) {
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
+                rightprevbtn = document.createElement("div");
+                rightprevbtn.classList.add("swiper-button-next")
+                block.append(rightprevbtn)
+
+                if (decryptObj[itemclass]['navigation'] != undefined) {
+                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn    
                 }else{
-                    decryptObj[itemclass]['pagination'] = {}
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
+                    decryptObj[itemclass]['navigation'] = {}
+                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
                 }
 
             }else if (itemclass == "slidepreviewauto") {
--- blocks/swiper/swiperConfig.js
+++ blocks/swiper/swiperConfig.js
@@ -19,13 +19,12 @@ let objClassSwipper = {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     }
   },
   "slidepreviewauto": {
-    loop: true,
     slidesPerView: "auto",
     spaceBetween: 30,
     pagination: {
