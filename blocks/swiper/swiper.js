997e8395e2760146181a5c146f0ce80bd4bbbb36
Ashwin Jethawa <Ashwin.Jethawa@teknopoint.local>
2025-06-16 16:58:25 +0530

    Added Changes

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -1,110 +1,130 @@
 import SwipperText from "../swiper/swiper-bundle.min.js"
 import swipperConfig from "./swiperConfig.js"
+const mobile = 480;
 export default function decorate(block) {
-    block.classList.add("swiper");
-    const swipperwrapper = document.createElement("div");
-    swipperwrapper.classList.add("swiper-wrapper");
+  var obj = {}
+  block.classList.add("swiper");
+  const swipperwrapper = document.createElement("div");
+  swipperwrapper.classList.add("swiper-wrapper");
+  Array.from(block.children).forEach((eleChild) => {
+    eleChild.classList.add("swiper-slide");
+    swipperwrapper.append(eleChild);
+  })
+  block.append(swipperwrapper);
+  
+  let leftprevbtn, rightprevbtn, swiperPagination;
+  let config = Array.from(block.classList).filter((itemclass) => {
+    if (itemclass) {
+      obj.pagination = {
+        el: swiperPagination,
+        clickable: true
+      }
+    }
+    if (itemclass == "mobile-pagination") {
+      obj.breakpoints[480].pagination = {
+        el: swiperPagination,
+        clickable: true
+      }
+    }
+    if (itemclass == "mobile-pagination") {
+      obj.pagination = {
+        el: swiperPagination,
+        clickable: true
+      }
+    }
+    let decryptObj = swipperConfig; //[itemclass];//JSON.parse(window.btoa(swipperConfig[itemclass]))
+    if (decryptObj[itemclass] != undefined) {
+      if (itemclass == "navigation") {
+        leftprevbtn = document.createElement("div");
+        leftprevbtn.classList.add("swiper-button-prev")
+        block.append(leftprevbtn)
 
-    Array.from(block.children).forEach((eleChild)=>{
-        eleChild.classList.add("swiper-slide");
-        swipperwrapper.append(eleChild);
-    })
-    block.append(swipperwrapper);
-    let leftprevbtn,rightprevbtn,swiperPagination;
-    let config = Array.from(block.classList).filter((itemclass)=>{
-        let decryptObj  = swipperConfig;//[itemclass];//JSON.parse(window.btoa(swipperConfig[itemclass]))
-         if(decryptObj[itemclass] != undefined){
-            if (itemclass == "navigation") {
-                leftprevbtn = document.createElement("div");
-                leftprevbtn.classList.add("swiper-button-prev")
-                block.append(leftprevbtn)
+        rightprevbtn = document.createElement("div");
+        rightprevbtn.classList.add("swiper-button-next")
+        block.append(rightprevbtn)
 
-                rightprevbtn = document.createElement("div");
-                rightprevbtn.classList.add("swiper-button-next")
-                block.append(rightprevbtn)
+        if (decryptObj[itemclass]['navigation'] != undefined) {
+          decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+          decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
+        } else {
+          decryptObj[itemclass]['navigation'] = {}
+          decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+          decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
+        }
 
-                if (decryptObj[itemclass]['navigation'] != undefined) {
-                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
-                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn    
-                }else{
-                    decryptObj[itemclass]['navigation'] = {}
-                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
-                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
-                }
-                
 
-            }else if (itemclass == "pagination") {
-                swiperPagination = document.createElement("div");
-                swiperPagination.classList.add("swiper-pagination");
-                block.append(swiperPagination);
+      } else if (itemclass == "pagination") {
+        swiperPagination = document.createElement("div");
+        swiperPagination.classList.add("swiper-pagination");
+        block.append(swiperPagination);
 
-                if (decryptObj[itemclass]['pagination'] != undefined) {
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
-                }else{
-                    decryptObj[itemclass]['pagination'] = {}
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
-                }
-                
-            }else if (itemclass == "slidepreview") {
-                leftprevbtn = document.createElement("div");
-                leftprevbtn.classList.add("swiper-button-prev")
-                block.append(leftprevbtn)
+        if (decryptObj[itemclass]['pagination'] != undefined) {
+          decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
+        } else {
+          decryptObj[itemclass]['pagination'] = {}
+          decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
+        }
 
-                rightprevbtn = document.createElement("div");
-                rightprevbtn.classList.add("swiper-button-next")
-                block.append(rightprevbtn)
+      } else if (itemclass == "slidepreview") {
+        leftprevbtn = document.createElement("div");
+        leftprevbtn.classList.add("swiper-button-prev")
+        block.append(leftprevbtn)
 
-                if (decryptObj[itemclass]['navigation'] != undefined) {
-                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
-                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn    
-                }else{
-                    decryptObj[itemclass]['navigation'] = {}
-                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
-                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
-                }
+        rightprevbtn = document.createElement("div");
+        rightprevbtn.classList.add("swiper-button-next")
+        block.append(rightprevbtn)
 
-            }else if (itemclass == "slidepreviewauto") {
-                swiperPagination = document.createElement("div");
-                swiperPagination.classList.add("swiper-pagination");
-                block.append(swiperPagination);
+        if (decryptObj[itemclass]['navigation'] != undefined) {
+          decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+          decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
+        } else {
+          decryptObj[itemclass]['navigation'] = {}
+          decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+          decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
+        }
 
-                if (decryptObj[itemclass]['pagination'] != undefined) {
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
-                }else{
-                    decryptObj[itemclass]['pagination'] = {}
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
-                }
+      } else if (itemclass == "slidepreviewauto") {
+        swiperPagination = document.createElement("div");
+        swiperPagination.classList.add("swiper-pagination");
+        block.append(swiperPagination);
 
-            }else if (itemclass == "breakpointswipper") {
-                swiperPagination = document.createElement("div");
-                swiperPagination.classList.add("swiper-pagination");
-                block.append(swiperPagination);
+        if (decryptObj[itemclass]['pagination'] != undefined) {
+          decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
+        } else {
+          decryptObj[itemclass]['pagination'] = {}
+          decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
+        }
 
-                if (decryptObj[itemclass]['pagination'] != undefined) {
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
-                }else{
-                    decryptObj[itemclass]['pagination'] = {}
-                    decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
-                }
-                leftprevbtn = document.createElement("div");
-                leftprevbtn.classList.add("swiper-button-prev")
-                block.append(leftprevbtn)
+      } else if (itemclass == "breakpointswipper") {
+        swiperPagination = document.createElement("div");
+        swiperPagination.classList.add("swiper-pagination");
+        block.append(swiperPagination);
 
-                rightprevbtn = document.createElement("div");
-                rightprevbtn.classList.add("swiper-button-next")
-                block.append(rightprevbtn)
+        if (decryptObj[itemclass]['pagination'] != undefined) {
+          decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination    
+        } else {
+          decryptObj[itemclass]['pagination'] = {}
+          decryptObj[itemclass]['pagination']["el"] = swiperPagination; //pagination
+        }
+        leftprevbtn = document.createElement("div");
+        leftprevbtn.classList.add("swiper-button-prev")
+        block.append(leftprevbtn)
 
-                if (decryptObj[itemclass]['navigation'] != undefined) {
-                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
-                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn    
-                }else{
-                    decryptObj[itemclass]['navigation'] = {}
-                    decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
-                    decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
-                }
-            }
-            return decryptObj[itemclass]
-         }
-    })
-    SwipperText(block,swipperConfig[config[0]])
-} 
+        rightprevbtn = document.createElement("div");
+        rightprevbtn.classList.add("swiper-button-next")
+        block.append(rightprevbtn)
+
+        if (decryptObj[itemclass]['navigation'] != undefined) {
+          decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+          decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
+        } else {
+          decryptObj[itemclass]['navigation'] = {}
+          decryptObj[itemclass]['navigation']["nextEl"] = rightprevbtn
+          decryptObj[itemclass]['navigation']["prevEl"] = leftprevbtn
+        }
+      }
+      return decryptObj[itemclass]
+    }
+  })
+  SwipperText(block, swipperConfig[config[0]])
+}
