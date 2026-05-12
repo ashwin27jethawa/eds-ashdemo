9e9dcfec7ab795ed45b1f20873c8cd344bde955b
Suresh Jadhav <suresh.jadhav@deptagency.com>
2026-05-12 16:50:51 +0530

    new table block

--- blocks/tables/tables.js
+++ blocks/tables/tables.js
@@ -11,6 +11,8 @@ function buildCell(rowIndex) {
 }
 
 export default async function decorate(block) {
+    console.log(block);
+    
   const table = document.createElement('table');
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
